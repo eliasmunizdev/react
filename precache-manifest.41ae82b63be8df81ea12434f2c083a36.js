30f8eb151474eb315cb87b7757df59e50924882e
eliasmuniz <38499934+eliasmuniz@users.noreply.github.com>
2019-11-16 13:18:24 -0300

    Add files via upload

---
+++ precache-manifest.41ae82b63be8df81ea12434f2c083a36.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a595a491b9063e44ca0957281eb09957",
+    "url": "./index.html"
+  },
+  {
+    "revision": "731dc93f0869a0d9d8e2",
+    "url": "./static/css/main.18fc7064.chunk.css"
+  },
+  {
+    "revision": "67d2a5074243fdb0817b",
+    "url": "./static/js/2.14e66b2e.chunk.js"
+  },
+  {
+    "revision": "731dc93f0869a0d9d8e2",
+    "url": "./static/js/main.5b4ab5c6.chunk.js"
+  },
+  {
+    "revision": "df6a6ba5a245ad9b2993",
+    "url": "./static/js/runtime-main.c84bef28.js"
+  }
+]);
